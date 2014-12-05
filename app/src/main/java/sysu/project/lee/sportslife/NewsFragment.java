99d7a70f9274e4f952aec5c43d4078c3b9f15865
lijun24455 <395711070@qq.com>
2014-12-05 11:07:29 +0800

    error i don know

--- app/src/main/java/sysu/project/lee/sportslife/NewsFragment.java
+++ app/src/main/java/sysu/project/lee/sportslife/NewsFragment.java
@@ -1,5 +1,6 @@
 package sysu.project.lee.sportslife;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,12 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.File;
+
+import sysu.project.lee.sportslife.News.AppContext;
 import sysu.project.lee.sportslife.News.NewsListActivity;
+import sysu.project.lee.sportslife.Utils.ToastUtils;
+import sysu.project.lee.sportslife.Utils.URLs;
+import sysu.project.lee.sportslife.Utils.mApplication;
 
 
 public class NewsFragment extends Fragment {
 
+    private String url = null;
+    private Context mContext = null;
     private Button mButton;
 
 
@@ -22,13 +32,34 @@ public class NewsFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        mContext = mApplication.getContextObject();
+
         mButton = (Button) getView().findViewById(R.id.btn_neo);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                url = URLs.NEO_URL;
                 Intent intent = new Intent();
+                intent.putExtra("url", URLs.NEO_URL);
+
                 intent.setClass(getActivity(), NewsListActivity.class);
-                startActivity(intent);
+
+
+                File cache = SectionHelper.getSdCache(url);
+                if(cache.exists())
+                {
+                    startActivity(intent);
+                }
+                else
+                {
+                    if(!AppContext.isNetworkAvailable(mContext))
+                    {
+                        ToastUtils.show(mContext, "网络未打开");
+                        return;
+                    }
+                    //异步加载数据
+                    new LoadDataTask().execute(url);
+                }
             }
         });
 
